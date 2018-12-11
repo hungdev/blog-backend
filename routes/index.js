c174873c5b983a125c097d3f3a6dd824c230f42a
Cee <cee@Cees-MacBook-Pro.local>
2018-12-11 11:40:43 +0700

    crud user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-import express from 'express';
-var router = express.Router();
+// import express from 'express';
+// var router = express.Router();
+global.router = require('express').Router();
+var router = global.router;
+router = require('./users');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+var router = global.router;
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+import checkAuth from '../middleware/checkAuth'
+
+const User = require("../models/user");
+const UserController = require('../controllers/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/get-users", UserController.get_users);
+
+router.post("/signup", UserController.user_signup);
+
+router.post("/login", UserController.user_login);
+
+router.delete("/user/:userId", checkAuth, UserController.user_delete);
 
 module.exports = router;
